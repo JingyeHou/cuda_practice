e7ebacdd212969bf049bae638a6dff425e2fc775
JingyeHou <jingyehou@gamil.com>
2019-03-03 10:41:21 -0500

    add git ignore

--- first_cuda.cu
+++ first_cuda.cu
@@ -3,8 +3,21 @@
 #include <iostream>
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <stdlib.h>
+
+#define DATA_SIZE 1048576
+
+int data[DATA_SIZE];
 
 using namespace std;
+
+void GenerateNumbers(int *number, int size)
+{
+    for(int i = 0; i < size; i++) {
+        number[i] = rand() % 10;
+    }
+}
+
 bool InitCUDA()
 {
     int count;
@@ -38,6 +51,19 @@ bool InitCUDA()
     return true;
 }
 
+__global__ static void sumOfSquares(int *num, int* result,
+    clock_t* time)
+{
+    int sum = 0;
+    int i;
+    clock_t start = clock();
+    for(i = 0; i < DATA_SIZE; i++) {
+        sum += num[i] * num[i];
+    }
+
+    *result = sum;
+    *time = clock() - start;
+}
 
 int main()
 {
@@ -46,6 +72,28 @@ int main()
     }
 
     printf("CUDA initialized.\n");
+
+    GenerateNumbers(data, DATA_SIZE);
+
+    int* gpudata, *result;
+        clock_t* time;
+        cudaMalloc((void**) &gpudata, sizeof(int) * DATA_SIZE);
+        cudaMalloc((void**) &result, sizeof(int));
+        cudaMalloc((void**) &time, sizeof(clock_t));
+        cudaMemcpy(gpudata, data, sizeof(int) * DATA_SIZE,
+            cudaMemcpyHostToDevice);
+
+        sumOfSquares<<<1, 1, 0>>>(gpudata, result, time);
+
+        int sum;
+        clock_t time_used;
+        cudaMemcpy(&sum, result, sizeof(int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&time_used, time, sizeof(clock_t),
+            cudaMemcpyDeviceToHost);
+        cudaFree(gpudata);
+        cudaFree(result);
+
+        printf("sum: %d time: %d\n", sum, time_used);
 
     return 0;
 }
